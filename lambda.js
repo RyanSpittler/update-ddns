4e26d99143f34ae28007d98d29ca9f10853c51f4
Recreation <recreation@Ryans-MacBook-Pro.local>
2017-12-08 11:41:57 -0700

    Initial changes

--- lambda.js
+++ lambda.js
@@ -1,22 +1,32 @@
-var config = require('./config.json');
-var soundBar = require("./lib/soundBar.js");
+var config = require("./config.json");
+var request = require("request");
+var crypto = require("crypto");
 
 // Entrypoint for AWS Lambda
 exports.handler = function(event, context) {
-  console.log('[EVENT] ', event);
+  console.log("[EVENT] ", event);
 
   // basic auth
   if(!isValidAuth(event.secret, event.key)) {
     return context.fail('Unauthorized request. Check authentication credentials.');
   }
 
-  // get input && respond
-  soundBar.setInput(event.text).then(function(response) {
-    context.succeed(response);
-  }).catch(function(reason) {
-    console.log('[FAIL] Unable to get current input: ', reason);
-    context.fail('Unable to get input. Try again later.');
-  });
+  // get ip address
+  var ip = event.sourceIP;
+  var password = config.dynu.password;
+  var hash = crypto.createHash("md5").update(password).digest("hex");
+  var domain = config.dynu.domainName;
+  var url = config.dynu.url + "?hostname=" + domain + "&myip=" + ip + "&password=" + hash
+  var options = {url};
+
+  request(options, function(err, response, body) {
+    if (err) {
+      console.log("[FAIL] Unable to set IP address for DDNS: ", reason);
+      context.fail("Unable to set IP address. Please troubleshoot.")
+    } else {
+      context.succeed(body)
+    } 
+  })
 };
 
 function isValidAuth(secret, key) {
